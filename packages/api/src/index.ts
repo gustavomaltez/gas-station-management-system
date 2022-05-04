b4e0fefaf1e75797bec3b5540c8bd9f8260f2c82
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-05-04 07:19:59 -0300

    fix(api): fixed small typo

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Well... Seems to be workiasdasdng!');
+  res.send('Well... Seems to be working!');
 });
 
 app.listen(7520, () => console.log('Listening on port 7520!'));
