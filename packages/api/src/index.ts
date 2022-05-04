f96c3e0031d693288cf357e9c715aa7fd36e6648
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-05-04 06:05:09 -0300

    feat(api): added log when api starts

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 const app = express();
- 
+
 app.get("/", function (req, res) {
   res.send("Well... Seems to be working!");
 });
- 
-app.listen(7520);
+
+app.listen(7520, () => console.log("Listening on port 7520!"));
