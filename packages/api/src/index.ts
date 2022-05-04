89e0957d1687ada935d94d62f7ff14df271b37e1
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-05-04 07:08:26 -0300

    chore(workspace): fixed husky scripts

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Well... Seems to be working!');
+  res.send('Well... Seems to be workiasdasdng!');
 });
 
 app.listen(7520, () => console.log('Listening on port 7520!'));
