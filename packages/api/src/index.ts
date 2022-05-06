5d1590ba9c9ce7ebf3e01c97c191cae8294e6ced
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-05-06 06:16:20 -0300

    feat(api): added logic to initialize database and expose authentication routes

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import { initialize as initializeDatabase } from './database/database';
+import { authenticationRouter } from './routes/authentication.route';
+import bodyParser from 'body-parser';
+
+initializeDatabase();
 const app = express();
-
-app.get('/', function (req, res) {
-  res.send('Well... Seems to be working!');
-});
-
+app.use(bodyParser.json());
+app.use('/auth', authenticationRouter);
 app.listen(7520, () => console.log('Listening on port 7520!'));
