57dbf6491837dbc7e1d1358b7c51b5d7d84729a2
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-05-10 06:03:27 -0300

    feat(api): added role property to User entity #1

--- packages/api/src/entities/User.entity.ts
+++ packages/api/src/entities/User.entity.ts
@@ -1,5 +1,10 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+export enum UserRole {
+  admin = 'admin',
+  employee = 'employee',
+}
+
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
@@ -13,4 +18,7 @@ export class User {
 
   @Column({ type: 'varchar', length: 100, unique: true })
   email: string;
+
+  @Column({ type: 'varchar', length: 25, default: UserRole.employee })
+  role: UserRole;
 }
