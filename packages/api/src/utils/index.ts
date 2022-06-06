ff71a4e30127c74c3152c3bb4fbff4228c50226a
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-06-06 08:18:06 -0300

    feat(api): added toArray util function

--- packages/api/src/utils/index.ts
+++ packages/api/src/utils/index.ts
@@ -20,3 +20,17 @@ export function hashString(string: string): string {
 export function hashedStringMatches(string: string, hashedString: string): boolean {
   return bcrypt.compareSync(string, hashedString);
 }
+
+/**
+ * Retrieves a item or an array of items and returns a array of items.
+ * 
+ * - This function is usefull when you need to make sure you are working with an
+ * data array, even if this array has only one item.
+ * 
+ * @param itemOrItems A single item or a array of items to convert to array.
+ * @returns A array of provided items.
+ */
+export function toArray<Item extends unknown>(itemOrItems: Item | Item[]): Item[] {
+  if (Array.isArray(itemOrItems)) return itemOrItems;
+  return [itemOrItems];
+}
