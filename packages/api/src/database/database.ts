aabcbdea4663172adf2f0ad50fa1e01211a0a040
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-06-06 08:08:01 -0300

    refactor(api): refactored database class in order to add sync loading

--- packages/api/src/database/database.ts
+++ packages/api/src/database/database.ts
@@ -20,36 +20,18 @@ const DEFAULT_DATABASE_CONNECTION_TRY_COUNT = 3;
  */
 class Database implements IDatabase {
 
-  private isLoading = false;
-  private isReady = false;
-  private connectionTryCount = 0;
-
   constructor(private readonly dataSource: DataSource) {
-    void this.initialize();
+    this.dataSource.initialize()
+    
+    // Methods binding ---------------------------------------------------------
+    this.getRepository = this.getRepository.bind(this);
   }
 
   // Public methods ------------------------------------------------------------
 
-  getRepository<EntityType>(entity: EntityTarget<EntityType>) {
+  async getRepository<EntityType>(entity: EntityTarget<EntityType>) {
+    if(!this.dataSource.isInitialized) await this.dataSource.initialize();
     return this.dataSource.getRepository<EntityType>(entity);
-  }
-
-  // Helper methods ------------------------------------------------------------
-
-  private async initialize(): Promise<void> {
-    if (this.isLoading || this.isReady) return;
-    this.isLoading = true;
-
-    this.connectionTryCount = this.connectionTryCount + 1;
-
-    try {
-      await this.dataSource.initialize();
-      this.isReady = true;
-    } catch (error) {
-      if (this.connectionTryCount > DEFAULT_DATABASE_CONNECTION_TRY_COUNT)
-        throw new Error(`Failed to initialize database connection: ${error}`);
-      return this.initialize();
-    }
   }
 }
 
