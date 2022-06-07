800fcdc6ca9c7d4812955f2d24a8bd1387ed8cdb
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-06-07 10:37:46 -0300

    feat(api): added isValidString util function

--- packages/api/src/utils/index.ts
+++ packages/api/src/utils/index.ts
@@ -34,3 +34,24 @@ export function toArray<Item extends unknown>(itemOrItems: Item | Item[]): Item[
   if (Array.isArray(itemOrItems)) return itemOrItems;
   return [itemOrItems];
 }
+
+/**
+ * Validates if a value is a valid string or not
+ * 
+ * - Note: The values considered as valid strings are only non empty strings
+ * 
+ * @example
+ * isValidString(null); // False
+ * isValidString(""); // False
+ * isValidString("  "); // False
+ * isValidString("example"); // True
+ * 
+ * @param value The value to check if is a valid string
+ * @returns Whether the provided value is a valid string or not
+ */
+export function isValidString(value: unknown) {
+  const isString = typeof value === "string";
+  if (!isString) return false;
+  const isNonNull = value.trim().length > 0;
+  return isNonNull;
+}
