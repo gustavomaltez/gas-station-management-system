3a9db3e55b323a75ca39cecffcdc21407e18e18c
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-06-09 08:32:28 -0300

    feat(api): added generateUserAccessToken util function

---
+++ packages/api/src/utils/authentication.ts
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+
+import { Administrator, Employee } from '../entities';
+
+/**
+ * Generates a string token associated to the provided user.
+ * 
+ * @param user The user data to generate the token for
+ * @returns A string token associated to the provided user
+ */
+export function generateUserAccessToken(user: Employee | Administrator): string {
+  const secret = process.env.ACCESS_TOKEN_SECRET as string;
+  const payload = { id: user.id, name: user.name };
+  return jwt.sign(payload, secret, { expiresIn: "1h" });
+}
