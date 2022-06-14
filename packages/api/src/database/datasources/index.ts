b4eee14d1701d4d414790146af00a86bbaa902c9
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-06-14 10:41:33 -0300

    feat(api): added postgres data source

--- packages/api/src/database/datasources/index.ts
+++ packages/api/src/database/datasources/index.ts
@@ -7,3 +7,14 @@ export const sqliteDataSource = new DataSource({
   synchronize: true,
   entities: [path.resolve(__dirname, '../../entities/*.entity.ts')],
 });
+
+export const postgresDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT ? Number(process.env.POSTGRES_PORT) : 5432,
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  synchronize: true,
+  entities: [path.resolve(__dirname, '../../entities/*.entity.ts')],
+});
