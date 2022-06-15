95266e3b37bfb7b5efa9e31ced09db979f383fae
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-06-15 08:45:46 -0300

    feat(api): added generateUserRefreshToken util function #1

--- packages/api/src/utils/authentication.ts
+++ packages/api/src/utils/authentication.ts
@@ -3,13 +3,29 @@ import jwt from 'jsonwebtoken';
 import { Administrator, Employee } from '../entities';
 
 /**
- * Generates a string token associated to the provided user.
+ * Generates a string access token associated to the provided user.
  * 
  * @param user The user data to generate the token for
  * @returns A string token associated to the provided user
  */
 export function generateUserAccessToken(user: Employee | Administrator): string {
-  const secret = process.env.ACCESS_TOKEN_SECRET as string;
+  const { ACCESS_TOKEN_SECRET, ACCESS_TOKEN_EXPIRATION_TIME } = process.env;
   const payload = { id: user.id };
-  return jwt.sign(payload, secret, { expiresIn: "1h" });
+  return jwt.sign(payload, ACCESS_TOKEN_SECRET as string, {
+    expiresIn: ACCESS_TOKEN_EXPIRATION_TIME
+  });
+}
+
+/**
+ * Generates a string refresh token associated to the provided user.
+ * 
+ * @param user The user data to generate the token for
+ * @returns A string token associated to the provided user
+ */
+export function generateUserRefreshToken(user: Employee | Administrator): string {
+  const { REFRESH_TOKEN_SECRET, REFRESH_TOKEN_EXPIRATION_TIME } = process.env;
+  const payload = { id: user.id };
+  return jwt.sign(payload, REFRESH_TOKEN_SECRET as string, {
+    expiresIn: REFRESH_TOKEN_EXPIRATION_TIME
+  });
 }
