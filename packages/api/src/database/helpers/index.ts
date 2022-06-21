9a754fcd3e3cd85f2e3c35cf7887bccca313eeb0
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-06-21 09:34:54 -0300

    feat(api): added basic database schema function

--- packages/api/src/database/helpers/index.ts
+++ packages/api/src/database/helpers/index.ts
@@ -7,16 +7,52 @@ import { Database } from '../types';
 
 export async function initializeDatabaseSchema(query: Database['query']) {
   await query(`
-    CREATE TABLE IF NOT EXISTS users (
+    CREATE TABLE IF NOT EXISTS employee (
       id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
       cpf VARCHAR(11),
       name VARCHAR(100),
       email VARCHAR(100),
       password VARCHAR(100),
       salary REAL,
-      street VARCHAR(100),
-      number REAL,
-      postal_code VARCHAR(20)
+      address_street VARCHAR(100),
+      address_number REAL,
+      address_postal_code VARCHAR(20),
+      is_admin_user BOOLEAN
+    );
+    
+    CREATE TABLE IF NOT EXISTS fuel (
+      id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+      liters_available REAL,
+      price_per_liter REAL,
+      name VARCHAR(20)
+    );
+    
+    CREATE TABLE IF NOT EXISTS client (
+      cpf VARCHAR(20) PRIMARY KEY NOT NULL,
+      full_name VARCHAR(100),
+      address_street VARCHAR(100),
+      address_number REAL,
+      address_postal_code VARCHAR(20),
+      birth_date DATE NOT NULL,
+      main_phone_number VARCHAR(20),
+      secondary_phone_number VARCHAR(20)
+    );
+    
+    CREATE TABLE IF NOT EXISTS vehicle (
+      registration_plate VARCHAR(20) PRIMARY KEY NOT NULL,
+      type VARCHAR(20),
+      brand VARCHAR(20),
+      client_id VARCHAR(20) references client(cpf)
+    );
+        
+    CREATE TABLE IF NOT EXISTS invoice (
+      id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+      issuance_date DATE DEFAULT CURRENT_DATE,
+      value REAL,
+      payment_method VARCHAR(20),
+      employee_id uuid references employee(id),
+      fuel_id uuid references fuel(id),
+      vehicle_id VARCHAR(20) references vehicle(registration_plate)
     );
   `);
 }
