f8c045b5894ca468d0c9c82df095ec71de611569
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-06-22 09:19:22 -0300

    refactor(api): refactored Employee entity in order to not allow undefined values

--- packages/api/src/entities/Employee.entity.ts
+++ packages/api/src/entities/Employee.entity.ts
@@ -4,8 +4,7 @@ interface Address {
   number: number;
 }
 
-export class Employee {
-  id: string;
+interface BaseEmployeeData {
   cpf: string;
   name: string;
   email: string;
@@ -13,4 +12,34 @@ export class Employee {
   salary: number;
   address: Address;
   isAdminUser: boolean;
+}
+export class Employee implements BaseEmployeeData {
+  private _id: string | null;
+  readonly cpf: string;
+  readonly name: string;
+  readonly email: string;
+  readonly password: string;
+  readonly salary: number;
+  readonly address: Address;
+  readonly isAdminUser: boolean;
+
+  constructor(data: BaseEmployeeData) {
+    this.address = data.address;
+    this.cpf = data.cpf;
+    this.email = data.email;
+    this.name = data.name;
+    this.password = data.password;
+    this.salary = data.salary;
+    this.isAdminUser = data.isAdminUser;
+    this._id = null;
+  }
+
+  get id(): string {
+    if (!this._id) throw new Error('Unable to retrieve the employee id. The id was not been set.');
+    return this._id;
+  }
+
+  set id(id: string) {
+    this._id = id;
+  }
 }
