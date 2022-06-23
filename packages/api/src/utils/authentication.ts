aacc3789dc2cd30bf4969793b91ee252c3dc70db
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-06-23 09:09:05 -0300

    refactor(api): refactored tokens generation functions #1

--- packages/api/src/utils/authentication.ts
+++ packages/api/src/utils/authentication.ts
@@ -3,45 +3,45 @@ import jwt from 'jsonwebtoken';
 import { Employee } from '../entities';
 
 /**
- * Generates a string access token associated to the provided user.
+ * Generates an access token associated to the provided employee.
  * 
- * @param user The user data to generate the token for
- * @returns A string token associated to the provided user
+ * @param employee The employee data to generate the token for.
+ * @returns An access token associated to the provided employee.
  */
-export function generateUserAccessToken(user: Employee): string {
+export function generateEmployeeAccessToken({ id }: Employee): string {
   const { ACCESS_TOKEN_SECRET, ACCESS_TOKEN_EXPIRATION_TIME } = process.env;
-  const payload = { id: user.id };
+  const payload = { id };
   return jwt.sign(payload, ACCESS_TOKEN_SECRET as string, {
     expiresIn: ACCESS_TOKEN_EXPIRATION_TIME
   });
 }
 
 /**
- * Generates a string refresh token associated to the provided user.
+ * Generates a refresh token associated to the provided employee.
  * 
- * @param user The user data to generate the token for
- * @returns A string token associated to the provided user
+ * @param employee The employee data to generate the token for.
+ * @returns A refresh token associated to the provided employee.
  */
-export function generateUserRefreshToken(user: Employee): string {
+export function generateEmployeeRefreshToken({ id }: Employee): string {
   const { REFRESH_TOKEN_SECRET, REFRESH_TOKEN_EXPIRATION_TIME } = process.env;
-  const payload = { id: user.id };
+  const payload = { id };
   return jwt.sign(payload, REFRESH_TOKEN_SECRET as string, {
     expiresIn: REFRESH_TOKEN_EXPIRATION_TIME
   });
 }
 
 /**
- * Helper function to generate both access and refresh tokens.
+ * Helper function to generate both access and refresh tokens for an employee.
  * 
- * @param user The user data to generate the tokens for
- * @returns An object containing both the access and refresh tokens
+ * @param employee The employee data to generate the token for.
+ * @returns An object containing both the access and refresh tokens.
  */
-export function generateUserTokens(user: Employee): {
+export function generateEmployeeTokens(employee: Employee): {
   accessToken: string,
   refreshToken: string;
 } {
   return {
-    accessToken: generateUserAccessToken(user),
-    refreshToken: generateUserRefreshToken(user)
+    accessToken: generateEmployeeAccessToken(employee),
+    refreshToken: generateEmployeeRefreshToken(employee)
   };
 }
