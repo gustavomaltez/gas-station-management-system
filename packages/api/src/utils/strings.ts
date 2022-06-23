70ee9115a480a944b3c300a86dace577e89d9b1f
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-06-23 09:41:59 -0300

    feat(api): added concatenateAndFormatStrings helper

--- packages/api/src/utils/strings.ts
+++ packages/api/src/utils/strings.ts
@@ -18,3 +18,23 @@ export function isValidString(value: unknown) {
   const isNonNull = value.trim().length > 0;
   return isNonNull;
 }
+
+/**
+ * Given an array of strings, concatenates all values into a single string.
+ * 
+ * @example
+ * concatenateStrings(['foo', 'bar', 'baz']) // 'foo, bar and baz'
+ * concatenateStrings(['foo', bar]) // 'foo and bar'
+ * concatenateStrings(['foo']) // 'foo'
+ * concatenateStrings([]) // ''
+ * 
+ * @param strings The list of strings to be concatenated.
+ * @returns A single string with all the values concatenated.
+ */
+export function concatenateAndFormatStrings(strings: string[]) {
+  if (strings.length === 0) return '';
+  if (strings.length === 1) return strings[0];
+  const lastString = strings.pop();
+  const formattedStrings = strings.join(', ');
+  return `${formattedStrings} and ${lastString}`;
+}
